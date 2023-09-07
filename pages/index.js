a78400ef69eac7e30039a4ebc1cc6442ea9b163b
agvan <agvandyke25@gmail.com>
2023-09-06 21:18:20 -0500

    built Hero component, attempted to get app to render locally

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
 import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
-import getLatestRepos from "@lib/getLatestRepos";
-import userData from "@constants/data";
+//import getLatestRepos from "..getLatestRepos/lib/getLatestRepos";
+//import userData from "@constants/data";
 
 export default function Home({ repositories }) {
     return (
@@ -24,7 +24,7 @@ export default function Home({ repositories }) {
     let token = process.env.GITHUB_AUTH_TOKEN;
   
     const repositories = await getLatestRepos(userData, token);
-    // console.log("REPOSITORIES", repositories);
+     //console.log("REPOSITORIES", repositories);
   
     return {
       props: {
