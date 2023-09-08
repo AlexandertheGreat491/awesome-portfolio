4646ee06e862522d39633ad96b84fd3ed1824591
agvan <agvandyke25@gmail.com>
2023-09-08 11:29:43 -0500

    added additional font weights to Roboto instance

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import {Roboto} from '@next/font/google'
 
 const roboto = Roboto ({
   subsets: ['latin'],
-  weight: '400',
+  weight: ['100', '300', '400', '500', '700']
 })
 
 export default function Home({ repositories }) {
